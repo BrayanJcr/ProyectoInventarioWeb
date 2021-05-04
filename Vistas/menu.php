b0056eabe0b082ee662a1e60be6a411b118a1d45
BrayanJcr <70727399+BrayanJcr@users.noreply.github.com>
2021-05-03 19:06:35 -0500

    Cambios de diseño

--- Vistas/menu.php
+++ Vistas/menu.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="js/scrollreveal.js"></script>
+    <title>Menu</title>
+    <script src="../js/scrollreveal.js"></script>
     <!--Bootstrap 5-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/login.css">
@@ -22,13 +23,13 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav clase-ul">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                <a class="nav-link active" aria-current="page" href="/">Inicio</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Productos</a>
+                <a class="nav-link" href="productos.php">Productos</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Ventas</a>
+                <a class="nav-link" href="ventas.php">Ventas</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -44,8 +45,6 @@
                 <a class="nav-link" href="#">Bienvenido</a>
               </li>
             </ul>
-            
-           
           </div>
         
       </nav>
@@ -76,6 +75,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     <!--script scroll reveal-->
-    <script src="js/scroll/loginscroll.js"></script>
+    <script src="../js/scroll/loginscroll.js"></script>
 </body>
 </html>
