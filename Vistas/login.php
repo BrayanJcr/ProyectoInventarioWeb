4e89deac32741b0cd3e3bad89b8f149bce8dd272
jim-ux <74369837@continental.edu.pe>
2021-05-03 19:12:31 -0500

    Minimal

--- Vistas/login.php
+++ Vistas/login.php
@@ -39,7 +39,7 @@
             </div>
             <button type="submit" class="btn btn-primary">Acceder</button>
             
-            <button type="submit" class="btn btn btn-success">Registrarse</button>
+            <button type="submit" class="btn btn btn-success margin-left-auto">Registrarse</button>
             
           </form>
 
