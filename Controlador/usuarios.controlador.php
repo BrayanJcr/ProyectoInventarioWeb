a739a05a6778a0ba73a8b0d60fadf5738ebb8fa0
Axel gutierrez palomino <75484126@continental.ed.pe>
2021-05-21 11:57:28 -0500

    User

--- Controlador/usuarios.controlador.php
+++ Controlador/usuarios.controlador.php
@@ -233,7 +233,7 @@ class ControladorUsuarios{
 	static public function ctrMostrarUsuarios($item, $valor){
 
 		$tabla = "usuarios";
-
+		include "../Modelos/productos.usuarios.modelo.php";
 		$respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $item, $valor);
 
 		return $respuesta;
